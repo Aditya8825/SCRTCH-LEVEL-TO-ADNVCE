ce4b4ff0949c2c4c3f9964aa3a8624d0b31c7e6a
Aditya Raj <aditya998887766@gmail.com>
2026-05-03 14:39:15 +0530

    factorialof number by method

---
+++ fac.java
@@ -0,0 +1,14 @@
+
+
+public class fac {
+    static int fact(int n){
+        int result=1;
+        for(int i=1; i<=n; i++){
+            result *=i;
+        }
+        return result;
+    }
+ public static void main(String[] args) {
+    System.out.println(fact(5));
+ }   
+}
