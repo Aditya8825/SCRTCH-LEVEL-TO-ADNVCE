1d56052c65345379b222dc97974cece485e98982
Aditya Raj <aditya998887766@gmail.com>
2026-05-03 14:38:47 +0530

    check even odd by method function

---
+++ evenodd.java
@@ -0,0 +1,14 @@
+// import java.util.Scanner;
+
+// public class evenodd{
+//     static void checkeven(int n){
+//         if(n%2==0) {
+//             System.out.println("even");
+//         }else{
+//             System.out.println("odd");
+      
+//     }}
+//     public static void main(String[] args) {
+        
+//         checkeven(9);
+//     }}
