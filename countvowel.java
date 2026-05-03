52a0eb5620a42af7dbe00711dc311548caa63c23
Aditya Raj <aditya998887766@gmail.com>
2026-05-03 14:38:14 +0530

    count vowels
    count vowels by creating ,method

---
+++ countvowel.java
@@ -0,0 +1,25 @@
+// public class countvowel {
+//      static void countvc(String str){
+//         int vowels=0;
+//         int  con=0;
+
+//         str=str.toLowerCase();
+
+//         for(int i=0; i<str.length(); i++){
+//         char ch=str.charAt(i);
+      
+//         if(ch=='a'|| ch=='e' || ch=='i'|| ch=='o'||ch=='u'){
+//           vowels++;
+//         }else{
+//             con++;
+//         }
+//      }
+//      System.out.println("vowel:"+vowels);
+//      System.out.println("cons:"+con);
+//     }
+    
+//     public static void main(String[] args) {
+//         String str="RAJa";
+//         countvc(str);
+//     }
+// }
