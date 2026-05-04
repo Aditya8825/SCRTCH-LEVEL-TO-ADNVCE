2f308a9efff2871c35ed42a612e8e7205c7d07c6
Aditya Raj <aditya998887766@gmail.com>
2026-05-04 23:12:08 +0530

    reverse element of array

---
+++ reversearr.java
@@ -0,0 +1,26 @@
+// public class reversearr {
+//     public static void main(String[] args) {
+//         int []arr={8,6,4,3,7};
+
+//        for(int i=arr.length-1; i>=0; i--){
+//         System.out.println(arr[i]+" ");
+
+
+//        }}}
+
+
+
+//method creating function<<<<<<<<<<<<<<<<<
+
+// public class revarr{
+//     static void revarr(int arr[]){
+//         for(int i=arr.length; i>=0; i--){
+//       System.out.print(arr[i]+" ");
+//         }
+//     }
+//     public static void main(String[] args) {
+//        int []arr={90,87,5,5,43,3};
+//        System.out.println("rev array:");
+//        revarr(arr);
+//     }
+// }
