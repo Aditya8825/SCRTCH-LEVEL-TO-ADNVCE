d1de3f8381fda58559b18bed0bb63b905628d3cf
Aditya Raj <aditya998887766@gmail.com>
2026-05-05 22:53:41 +0530

    search in array

---
+++ search.java
@@ -0,0 +1,16 @@
+// public class search{
+//     public static void main(String[] args) {
+//         int []arr={7,5,34,2,89,7,87,40};
+//         int target=89;
+//         boolean result=false;
+//         for(int i=0; i<arr.length; i++){
+//             if(arr[i]==target){
+//                 result=true;
+//                 break;
+//                 //System.out.println("taget is found");
+//             }
+//         }
+//         if(result)   System.out.println("target exist in array:"+target);
+//         else System.out.println("target missing in array:");
+//     }
+// }
