97ec8361b01624655fc4df279c85e0378eade305
Aditya Raj <aditya998887766@gmail.com>
2026-05-05 22:54:07 +0530

    remove dublicate element

---
+++ removedubplicate.java
@@ -0,0 +1,19 @@
+
+// public class removedubplicate {
+//    public static void main(String[] args) {
+//     int []arr={1,1,3,2,3,4,5};
+//     int j=0;
+
+//     for(int i=0; i<arr.length-1; i++){
+//        if(arr[i]!=arr[i+1]){
+//         arr[j++]=arr[i];
+      
+//        }
+//     }
+//        arr[j++]=arr[arr.length-1];
+//      System.out.println("arr :");
+//      for(int i=0; i<j; i++){
+//         System.out.println(arr[i]+" ");
+//      }}}
+
+  
