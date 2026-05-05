3de5e765093af3ace3d01807ca3158d3d355aec9
Aditya Raj <aditya998887766@gmail.com>
2026-05-05 22:52:28 +0530

    move to zeroes

---
+++ movetozero.java
@@ -0,0 +1,19 @@
+
+
+// public class movetozero {
+//   public static void main(String[] args) {
+//     int []arr={12,3,4,5,2,6,6,3,5};
+//     int j=0;
+
+// for(int i=0; i<arr.length; i++){
+//     if(arr[i]!=0){
+//         int temp=arr[i];
+//         arr[i]=arr[j];
+//         arr[j]=temp;
+//         j++;
+//     }
+// }
+// for(int i=0; i<arr.length; i++){
+//     System.out.println(arr[i]+" ");
+// }}
+// }
