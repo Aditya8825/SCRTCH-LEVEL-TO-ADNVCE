750291d390ed31c342c9f2a0c426898378546aa0
Aditya Raj <aditya998887766@gmail.com>
2026-05-09 16:51:35 +0530

    sum of all element in 2 d array

--- sumofarr.java
+++ sumofarr.java
@@ -1,27 +1,14 @@
-// public class sumaofallelement {
-//     public static void main(String[] args) {
-//         int []arr={1,2,3,3,5,6};
 
-//         int sum=0;
-//         for(int num : arr){
-//           sum+=num;
+
+// public class sumofarr {
+//    public static void main(String[] args) {
+//     int [][] arr={{2,1,2,3},{4,56,7,8},{9,8,7,6}};
+//     int sum=0;
+//     for(int i=0; i<arr.length; i++){
+//         for(int j=0; j<arr.length; j++){
+//             sum +=arr[i][j];
 //         }
-//         System.out.println("sum:"+sum);
 //     }
+//     System.out.println(sum);
+//    } 
 // }
-
-
-
-public class sumofarr{
-    static void sumArr(int arr[]){
-     int sum=0;
-          for(int i=0; i<arr.length; i++){
-            sum+=arr[i];
-          }
-          System.out.println("sum="+sum);
-        }
-    public static void main(String[] args) {
-            int arr[]={9,4,7,36,5};
-            sumArr(arr);
-          }
-    }
