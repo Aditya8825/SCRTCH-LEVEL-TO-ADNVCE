0756deef59300743ed606bb1e49e9c6a5c2b9fd2
Aditya Raj <aditya998887766@gmail.com>
2026-05-09 16:49:55 +0530

    maximum element in 2darray

---
+++ maxelementinrow.java
@@ -0,0 +1,53 @@
+
+// // public class maxelementinrow {
+    
+
+
+// //     public static void main(String[] args) {
+// //         int [][]arr={{1,2,3,4},{5,6,7,8},{98,90,88,99}};
+// //         int maxSum = Integer.MIN_VALUE;
+// //         for(int i=0; i<arr.length; i++){
+// //             int sum=0;
+             
+// //             for(int j=0; j<arr.length; j++){
+// //               sum+=arr[i][j];
+// //             }
+// //             maxSum=Math.max(maxSum, sum);
+// //         }
+            
+    
+// //         System.out.print("max element ->"+maxSum);
+// //     }
+// // }
+
+
+
+
+
+// // also print index<<<<<<<<<<<<<<<<<<<<<
+
+// public class maxelementinrow {
+    
+
+
+//     public static void main(String[] args) {
+//         int [][]arr={{1,2,3,4},{5,6,7,8},{98,90,88,99}};
+//         int maxSum = Integer.MIN_VALUE;
+//         int row=-1;
+//         for(int i=0; i<arr.length; i++){
+//             int sum=0;
+             
+//             for(int j=0; j<arr.length; j++){
+//               sum+=arr[i][j];
+//             }
+//           //  maxSum=Math.max(maxSum, sum);
+//           if(sum>maxSum){
+//             maxSum=sum;
+//             row=i;
+//           }
+//         }
+            
+    
+//         System.out.print("row->"+row+" max element ->"+maxSum);
+//     }
+// }
