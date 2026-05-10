f6456e3caaffb0319962af60953d466eaabe0908
Aditya Raj <aditya998887766@gmail.com>
2026-05-11 01:00:44 +0530

    count no word
    no of word in string

---
+++ countword.java
@@ -0,0 +1,27 @@
+
+// public class countword {
+//  public static void main(String[] args) {
+//     String str="aditya";
+//     int count=0;
+//     //count whole string<<<<<<<<<<<<<<<<
+     
+// //     String[]word=str.split(" ");
+// //     System.out.println(word.length);
+// //  }   
+// // }
+
+
+
+// // count whole string<<<<<<<<<,,
+// // for(char ch: str.toCharArray()){
+// //    count++;
+// // }
+// //  System.out.println(count);
+// // }}
+
+
+// for(int i=0; i<=str.length()-1; i++){
+//    count++;
+// }
+// System.out.println(count);
+//  }}
