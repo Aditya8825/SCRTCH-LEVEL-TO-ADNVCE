e07f5c54830ea1b19d65f690a2995ff73e926f63
Aditya Raj <aditya998887766@gmail.com>
2026-05-10 21:45:35 +0530

    matric formatio by multiplaction

---
+++ multiplicationofarr.java
@@ -0,0 +1,35 @@
+
+// public class multiplicationofarr {
+//     public static void main(String[] args) {
+//         int [][]a={
+//             {1,2,3},
+//             {4,5,6},
+//             {7,8,9}
+//         };
+//          int [][]b={
+//             {9,8,7},
+//             {7,6,5},
+//             {1,2,3}
+//          };
+//          int [][]c= new int[3][3];
+//          for(int i=0; i<a.length; i++){
+//             for(int j=0; j<a[0].length; j++){
+//                 for(int k=0; k<b.length; k++){
+//                    c[i][j]+= a[i][j]* b[i][j];
+//                 }
+//             }
+//          }
+//          for(int i=0; i<c.length; i++){
+//             for(int j=0; j<c[i].length; j++){
+//                 System.out.print(c[i][j]+" ");
+//             }
+//             System.out.println();
+//          }
+//         }}
+
+
+     
+            
+
+
+            
