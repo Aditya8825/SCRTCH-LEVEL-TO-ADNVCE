665a46b90caac1812804f02e3960861a3e4f04e6
Aditya Raj <aditya998887766@gmail.com>
2026-05-10 21:45:00 +0530

    seach element in sorted array

---
+++ searchsortedmatric.java
@@ -0,0 +1,55 @@
+
+// public class searchsortedmatric{
+//     public static void main(String[] args) {
+
+//         int[][] arr = {
+//                 {1, 2, 3, 4},
+//                 {5, 6, 7, 8},
+//                 {9, 1, 7, 5},
+//                 {4, 3, 2, 1}
+//         };
+//       int target=5;
+//     //   int row=0;
+//     //   int col=arr[0].length-1;
+
+// //       while(row<arr.length && col>=0){
+// //            if(arr[row][col]==target){
+// //             System.out.println("found");
+// //             return;
+// //            }
+// //            else if(arr[row][col]>target){
+// //             col--;
+// //            }
+// //       else{
+// //         row++;
+// //       }
+// //         }
+// //         System.out.println("not found");
+// // }
+// // }
+
+
+
+
+// int i=0;
+// int j=arr[0].length-1;
+
+// while(i<arr.length && j>=0){
+//     if(arr[i][j]==target){
+//         System.out.println("elment found");
+//         return;
+//     }
+//  else if (arr[i][j] > target) {
+//                 j--;
+//             }
+
+//             // move down
+//             else {
+//                 i++;
+//             }
+//         }
+
+//         System.out.println("Element Not Found");
+//     }
+// }
+
