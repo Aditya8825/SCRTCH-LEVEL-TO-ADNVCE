bf4855141e4e3d5c091d2cc49d36f35cc29c718b
Aditya Raj <aditya998887766@gmail.com>
2026-05-11 01:01:58 +0530

    lowercaseto uperr case

---
+++ lowercasetoupeer.java
@@ -0,0 +1,13 @@
+// public class lowercasetoupeer {
+//     public static void main(String[] args) {
+//         String str="java";
+//         String res="";
+//         for(char c:str.toCharArray()){
+//             if(c>='a' && c<='z')
+//               c=(char)(c-32);
+
+//               res +=c;
+//         }
+//         System.out.println(res);
+//     }
+// }
