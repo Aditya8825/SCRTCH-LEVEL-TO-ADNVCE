9c4984e020ebf9c3fdb8b6a038123bc376c17366
Aditya Raj <aditya998887766@gmail.com>
2026-05-11 01:04:11 +0530

    length of string

---
+++ stringlength.java
@@ -0,0 +1,26 @@
+
+
+public class stringlength {
+    public static void main(String[] args) {
+        String str="Aditya";
+        int count=0;
+        //with using length<<<<<<<<<<<<<<<
+
+//         for(int i=str.length()-1;  i>=0; i--){
+//      count++;
+//         }
+//         System.out.println("length of :"+ str + count);
+//     }
+// }
+
+
+//without using length
+try{
+    while(true){
+        str.charAt(count);
+        count++;
+    }
+    
+}catch(Exception e){}
+    System.out.println("length: "+count);
+}}
