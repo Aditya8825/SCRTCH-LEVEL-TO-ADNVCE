fb488eb2f659d32ce4cbb4b6f129903dd34693a2
Aditya Raj <aditya998887766@gmail.com>
2026-05-12 22:34:56 +0530

    countsubstring

---
+++ countaallsubstring.java
@@ -0,0 +1,22 @@
+
+
+// public class countaallsubstring {
+//     public static void main(String[] args) {
+//         String str="abc";
+//         int n=str.length();
+//         int count=0;
+
+//         for(int i=0; i<n; i++){
+//             for(int j=i; j<n; j++){
+//                 count++;
+
+//                 for(int k=i; k<=j; k++){
+//                     System.out.println(str.charAt(k));
+//                 }
+
+//                 System.out.println();
+//             }
+//         }
+//         System.out.print("total subtring="+count);
+//     }
+// }
