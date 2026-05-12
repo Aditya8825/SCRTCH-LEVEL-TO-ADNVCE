58ef6ea37851185b8971ffbcd74e9cc916cba934
Aditya Raj <aditya998887766@gmail.com>
2026-05-12 22:35:35 +0530

    check palndrome in substring

---
+++ palindromeofsubstring.java
@@ -0,0 +1,18 @@
+
+// public class palindromeofsubstring {
+//     public static boolean isplaindrome(String str,int start,int end){
+//         while(start<end){
+//             if(str.charAt(start)!=str.charAt(end)){
+//                 return false;
+//             }
+//             start++;
+//             end--;
+//         }
+//         return true;
+//     }
+//     public static void main(String[] args) {
+//         String str="madam";
+//     boolean ans=isplaindrome(str, 0, str.length()-1);
+//     System.out.println(ans);
+//     }
+// }
