ef7a93fbf4a6e73a73354cc3c608d1a70b238ed4
Aditya Raj <aditya998887766@gmail.com>
2026-05-12 22:36:03 +0530

    reverse of substring

---
+++ reverse.java
@@ -0,0 +1,19 @@
+
+
+// public class reverse{
+//     public static void main(String[] args) {
+//         String s="Devesh";
+//         StringBuilder sb =new StringBuilder(s);
+//         sb.append("jain");
+//         System.out.println(sb);
+//     }
+// }
+
+
+
+// String s="Aditya";
+// StringBuilder sb=new StringBuilder(s);
+// sb.reverse();
+// System.out.println(sb);
+//     }}
+
