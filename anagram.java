85d5acbae6396885ba897209631dcbe3afb0d2e7
Aditya Raj <aditya998887766@gmail.com>
2026-05-12 22:34:30 +0530

    Anagram

---
+++ anagram.java
@@ -0,0 +1,27 @@
+// import java.util.ArrayList;
+
+// import javax.print.DocFlavor.INPUT_STREAM;
+
+// public class anagram {
+//     public static boolean issame(int[] arr1,int[]arr2){
+//         for(int i=0; i<26; i++){
+//             if(arr1[i]!=arr2[i]){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+//     public static ArrayList<Integer>findAngrams(String text,String Pattern){
+
+//     }
+//     public static void main(String[] args) {
+//         String text="cbaebabacd";
+//         String pattern="abc";
+
+//         ArrayList<Integer>ans = findAnagrams(text,pattern);
+//         System.out.println("anagram indices:");
+//         for(int i=0; i<ans.size(); i++){
+//             System.out.println(ans.get(i)+" ");
+//         }
+//     }
+// }
