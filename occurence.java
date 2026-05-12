4af154ec3ccd2920fd1f12c084298d17fd1a6f94
Aditya Raj <aditya998887766@gmail.com>
2026-05-13 00:20:20 +0530

    no of occurence in string

---
+++ occurence.java
@@ -0,0 +1,11 @@
+
+public class occurence {
+    public static void main(String[] args) {
+        String str="i am learning programming";
+        if(str.contains("learning")){
+            System.out.println("substring found");
+        }else{
+            System.out.println("not found");
+        }
+    }
+}
