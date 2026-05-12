cadbabed2d64d22de705a4b94939d9a5e73d71fd
Aditya Raj <aditya998887766@gmail.com>
2026-05-12 22:36:32 +0530

    Substring

---
+++ Strings.java
@@ -0,0 +1,10 @@
+
+// public class Strings {
+//    public static void main(String[] args) {
+//     String s="aditya";
+//     StringBuilder sb=new StringBuilder(s);
+//     sb.reverse();
+//     s=sb.toString();
+//     System.out.println(s);
+//    } 
+// }
