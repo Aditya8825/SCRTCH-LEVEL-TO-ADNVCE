8c3afd564cb1a52508a1f19df2ddb2b85248b2dd
Aditya Raj <aditya998887766@gmail.com>
2026-05-13 00:22:38 +0530

    substring

---
+++ substring.java
@@ -0,0 +1,19 @@
+
+// public class substring {
+//  public static void main(String[] args) {
+//     String str="raj";
+//    // System.out.println(str.substring(2));
+//    //.out.println(str.substring(2,4));
+
+// //    for(int i=1; i<=s.length(); i++){
+// //     System.out.println(s.substring(0,i));
+
+
+// for(int i=0; i<=str.length(); i++){
+//     for(int j=i+1; j<=str.length(); j++){
+//         System.out.print(str.substring(i,j));
+//     }
+//     System.out.println();
+// }
+//    }
+//  }   
