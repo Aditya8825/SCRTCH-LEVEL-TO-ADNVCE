05d4398b439f29701c5e4c8d0c45c7bbafd9c17a
Aditya Raj <aditya998887766@gmail.com>
2026-05-12 22:37:03 +0530

    sum of substring

---
+++ sumofsubstring.java
@@ -0,0 +1,17 @@
+// // package SUBSTRING;
+
+// public class sumofsubstring {
+//    public static void main(String[] args) {
+//     String num="123";
+//     int sum=0;
+//     for(int i=0; i<num.length(); i++){
+//            String temp="";
+
+//            for(int j=i; j<num.length();j++){
+//             temp+=num.charAt(j);
+//             sum +=Integer.parseInt(temp);
+//            }
+//     }
+//     System.out.println("sum="+sum);
+//    } }
+
