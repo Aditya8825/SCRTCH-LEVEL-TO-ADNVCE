fca90cdf1e541577362035c225724cc148d3c012
Aditya Raj <aditya998887766@gmail.com>
2026-05-13 23:29:48 +0530

    basic searching in jva

--- search.java
+++ search.java
@@ -1,16 +1,27 @@
 // public class search{
-//     public static void main(String[] args) {
-//         int []arr={7,5,34,2,89,7,87,40};
-//         int target=89;
-//         boolean result=false;
-//         for(int i=0; i<arr.length; i++){
-//             if(arr[i]==target){
-//                 result=true;
-//                 break;
-//                 //System.out.println("taget is found");
+//     static int search(int []arr,int target){
+//         int left=0;
+//         int right=arr.length-1;
+
+//         while(left<=right){
+//             int mid=(left+right)/2;
+
+//             //element found
+//             if(arr[mid]==target){
+//                 return mid;
+//             }
+//             if(target<arr[mid]){
+//                 right=mid-1;
+//             }
+//             else{
+//                 left=mid+1;
 //             }
 //         }
-//         if(result)   System.out.println("target exist in array:"+target);
-//         else System.out.println("target missing in array:");
+//         return -1;
+//     }
+//     public static void main(String[] args) {
+//         int []arr={1,2,3,4,5,6,7,7,8,9};
+//         int result = search(arr,7);
+//         System.out.println("index of "+7+" is = "+result);
 //     }
 // }
