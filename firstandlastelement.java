40a5a8e9cd36ba49e238876ffe60e5ffccbda7cb
Aditya Raj <aditya998887766@gmail.com>
2026-05-15 23:13:57 +0530

    first and last element

---
+++ firstandlastelement.java
@@ -0,0 +1,11 @@
+package BINARYSEACHRING2;
+
+import java.util.Arrays;
+
+public class firstandlastelement {
+    public static void main(String[] args) {
+        int []arr={5,7,7,8,8,10};
+        int target=10;
+        System.out.println(Arrays.toString(searchrange(arr,target)));
+    }
+}
