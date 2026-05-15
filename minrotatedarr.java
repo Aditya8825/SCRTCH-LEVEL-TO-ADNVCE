3f27536e5a5cb76fbf8782e618d48515beed6a7c
Aditya Raj <aditya998887766@gmail.com>
2026-05-15 23:15:24 +0530

    ,min order element

---
+++ minrotatedarr.java
@@ -0,0 +1,23 @@
+
+public class minrotatedarr {
+    static int findmin(int[]arr){
+        int left=0;
+        int right=arr.length-1;
+
+        while(left<right){
+            int mid=(left+right)/2;
+
+            //minimum  is in right
+            if(arr[mid]>arr[right]){
+                left=mid+1;
+            }else{
+                right=mid;
+            }
+        }
+        return arr[left];
+    }
+    public static void main(String[] args) {
+        int []arr={4,5,6,7,0,1,2};
+        System.out.println("minimum element: "+findmin(arr));
+    }
+}
