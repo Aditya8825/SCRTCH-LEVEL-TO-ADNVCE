23cbd2596f9640e2958b36e05cb1116bed038375
Aditya Raj <aditya998887766@gmail.com>
2026-05-16 23:38:18 +0530

    basic of sorting

---
+++ basicofsorting.java
@@ -0,0 +1,18 @@
+
+// public class basicofsorting {
+//     static boolean arraysorted(int []arr){
+//         for(int i=0; i<arr.length-1; i++){
+//             if(arr[i]>arr[i+1]){
+//                 return false;
+//             }
+//            // return true;
+//         }
+//         return true;
+//     }
+//    public static void main(String[] args) {
+//     int []arr={1,2,3,0,5};
+//    System.out.println(arraysorted(arr));
+      
+
+//    } 
+// }
