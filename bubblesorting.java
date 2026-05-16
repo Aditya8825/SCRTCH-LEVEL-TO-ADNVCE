f7934f4ed2e4cb52011b1ef9322010f7a0db2271
Aditya Raj <aditya998887766@gmail.com>
2026-05-16 23:38:44 +0530

    bubblesortingofarr

---
+++ bubblesorting.java
@@ -0,0 +1,20 @@
+// public class bubblesorting{
+//     static void sorting(int[]arr){
+//         for(int j=0; j<arr.length-j; j++){
+//           for(int i=0; i<arr.length-1-j; i++){
+//             if(arr[i]>arr[i+1]){
+//                int  temp=arr[i];
+//                  arr[i]=arr[i+1];
+//                  arr[i+1]=temp;
+//             }
+//           }
+//         }
+//     }
+//     public static void main(String[] args) {
+//         int []arr={0,8,5,7,-5,1,4,-1};
+//         sorting(arr);
+//         for(int i=0; i<arr.length; i++){
+//             System.out.print(arr[i]+" ");
+//         }
+//     }
+// }
