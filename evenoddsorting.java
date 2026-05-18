3b7c478c7b2b039c3da17a7b778cafee23a47169
Aditya Raj <aditya998887766@gmail.com>
2026-05-18 09:47:23 +0530

    even odd number sorting using binary sorting

---
+++ evenoddsorting.java
@@ -0,0 +1,61 @@
+
+// public class evenoddsorting {
+// //   static void rearrange(int[]arr){
+// //     for(int i=0; i<arr.length-1; i++){
+// //         for(int j=0; j<arr.length-1-i; j++){
+
+// //             if(arr[j]%2!=0   && arr[j+1]%2==0){
+
+// //                 int temp=arr[j];
+// //                 arr[j]=arr[j+1];
+// //                 arr[j+1]=temp;
+
+// //             }
+// //         }
+// //     }
+// //   }
+// //     public static void main(String[] args) {
+// //         int[]arr= {1, 4, 3, 2, 7, 6, 9};
+// //          rearrange(arr);
+// //         // for(int i=0; i<arr.length; i++){
+// //         //     System.out.print(arr[i]+" ");
+
+// //          for (int num : arr) {
+// //             System.out.print(num + " ");
+// //         }
+
+// //     }
+// // }
+
+
+
+
+// //public class EvenOddBubbleSort {
+
+//     static void rearrange(int[] arr) {
+
+//         for (int i = 0; i < arr.length - 1; i++) {
+
+//             for (int j = 0; j < arr.length - 1 - i; j++) {
+
+//                 if (arr[j] % 2 != 0 && arr[j + 1] % 2 == 0) {
+
+//                     int temp = arr[j];
+//                     arr[j] = arr[j + 1];
+//                     arr[j + 1] = temp;
+//                 }
+//             }
+//         }
+//     }
+
+//     public static void main(String[] args) {
+
+//         int[] arr = {1, 4, 3, 2, 7, 6, 9};
+
+//         rearrange(arr);
+
+//         for (int num : arr) {
+//             System.out.print(num + " ");
+//         }
+//     }
+// }
