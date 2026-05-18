78edb0a21fab9c4782b130a4d04e91515ab145e6
Aditya Raj <aditya998887766@gmail.com>
2026-05-18 09:47:51 +0530

    secon largest element using BINARY SORTIG

---
+++ secondlargest.java
@@ -0,0 +1,26 @@
+
+// public class secondlargest {
+   
+//     static int arraysorted(int []arr){
+//         for(int i=0; i<arr.length-1; i++){
+//             for(int j=0; j<arr.length-1-i; j++){
+//             if(arr[j]>arr[j+1]){
+
+//                int temp=arr[j];
+//                arr[j]=arr[j+1];
+//                arr[j+1]=temp;
+//             }
+//         }
+//         }
+//         //return arr[arr.length-2];
+//         return arr[arr.length-1];
+//     }
+//    public static void main(String[] args) {
+//     int []arr={1,2,3,0,5};
+//    System.out.println(arraysorted(arr));
+      
+
+//    } 
+// }
+
+    
