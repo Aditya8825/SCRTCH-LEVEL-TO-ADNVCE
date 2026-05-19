672df10e3b465898bb48cb2f2463713f6a2f42bd
Aditya Raj <aditya998887766@gmail.com>
2026-05-19 15:52:34 +0530

    largestelementofinselection

---
+++ largetest.java
@@ -0,0 +1,16 @@
+// public class largetest {
+//     static void findmax(int []arr){
+//         int max=arr[0];
+//         for(int i=1; i<arr.length; i++){
+//            if(arr[i]>max){
+//             max=arr[i];
+//            }
+//         }
+        
+//         System.out.println("maximum element: "+max);
+//     }
+//    public static void main(String[] args) {
+//     int[]arr={5,6,7,8,1};
+//     findmax(arr);
+//    } 
+// }
