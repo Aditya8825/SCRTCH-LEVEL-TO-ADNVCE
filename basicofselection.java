b5cb9fabb080e05e5462c778422dbe5032f73cc8
Aditya Raj <aditya998887766@gmail.com>
2026-05-19 15:49:21 +0530

    basicofselection

---
+++ basicofselection.java
@@ -0,0 +1,42 @@
+// // public class basicofselection{
+// //     static void   selection(int[]arr){
+// //         for(int i=0; i<arr.length-1; i++){
+
+// //              int min=Integer.MAX_VALUE,mindx=-1;
+
+// //             for(int j=i;j<arr.length; j++){
+// //                 if(arr[j]<min){
+// //                     min=arr[j];
+// //                     mindx=j;
+// //                 }
+// //             }
+// //             int temp=arr[i];
+// //             arr[i]=arr[mindx];
+// //             arr[mindx]=temp;
+// //         }
+// //     }
+// //     public static void main(String[] args) {
+// //         int []arr={1,8,2,9,19,6,5,6,3};
+// //         selection(arr);
+// //         for(int i=0; i<arr.length; i++){
+// //             System.out.print(arr[i]+" ");
+// //         }
+// //     }
+// // }
+
+// import java.util.stream.Gatherer.Integrator;
+
+// public class basicofselection{
+//     static void selection(int[]arr){
+//         for(int i=0 ;i<arr.length; i++){
+//         int main=Integer.MAX_VALUE,mindx=-1;
+//         for(int j=i; j<arr.length; j++){
+//             if(arr[j])
+//         }
+//     }
+// }
+//     public static void main(String[] args) {
+//     int []arr={1,2,8,6,3,6,8,6,3,0};
+
+//     }
+// }
