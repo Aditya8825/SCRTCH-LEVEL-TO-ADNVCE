1fdbde5b09006ed408cd1fe0c32debc66c33cb64
Aditya Raj <aditya998887766@gmail.com>
2026-05-19 15:51:16 +0530

    basicofinsertion

---
+++ frombasic.java
@@ -0,0 +1,34 @@
+// public class frombasic {
+    
+
+
+
+// public class basicofselection {
+//     static void selectionsorting(int []arr){
+//         //selction sortonlyeven
+//         for(int i=0; i<arr.length-1; i++){
+//             int minIndex=i;
+
+//             //find smallest element
+//             for(int j=i; j<arr.length; j++){
+//                 if(arr[j]<arr[minIndex]){
+//                     minIndex=j;
+//                    // minIndex=j;
+//                 }
+//             }
+//             // swaps
+//             int temp=arr[i];
+//             arr[i]=arr[minIndex];
+//             arr[minIndex]=temp;
+
+//         }
+//     }
+//     public static void main(String[] args) {
+//         int []arr={64,25,12,22,1,2,3};
+//         selectionsorting(arr);
+//         for(int i=0; i<arr.length; i++){
+//             System.out.print(arr[i]+" ");
+//         }
+//     }
+// }
+// }
