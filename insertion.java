038da92a482e8190f26e4bc62b5b13753e6e2c10
Aditya Raj <aditya998887766@gmail.com>
2026-05-20 22:54:39 +0530

    basic of insertion sorting

---
+++ insertion.java
@@ -0,0 +1,50 @@
+// public class insertion {
+//     static void insertionsorting(int[]arr){
+//         for(int i=1; i<arr.length; i++){
+//            int count=0;
+//             int j=i;
+
+//             while(j>0 && arr[j]<arr[j-1]){
+//                 int temp=arr[j];
+//                 arr[j]=arr[j-1];
+//                 arr[j-1]=temp;
+//                 j--;
+//                 count++;
+//             }
+             
+//         }
+      
+//      //   System.out.println(insertion(arr));
+//     }
+//     public static void main(String[] args) {
+//         int[]arr={4,1,7,3,9,2,0,8};
+
+//          insertionsorting(arr);
+
+//          for(int i=0; i<arr.length; i++){
+//          System.out.print(arr[i]+" ");
+//         }
+//     }
+// }
+
+
+
+// // public class insertion{
+// //     static void insertion(int[]arr){
+// //         for(int i=1; i<arr.length; i++){
+// //             int j=i;
+// //             while( j>0 && arr[j]<arr[j-1] ){
+// //                 int temp=arr[j];
+// //                 arr[j]=arr[j-1];
+// //                 arr[j-1]=temp;
+// //                 j--;
+// //             }                                                                                                                            )
+// //         }
+// //     }
+// //     public static void main(String[] args) {
+// //     int[]arr={4,1,7,3,9,0,8};
+// //         insertion(arr);
+// //         System.out.println("total");
+// //     }
+
+// // }
