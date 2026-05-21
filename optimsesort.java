714dcae2e1405a92dc2199d2e8688291cc518f52
Aditya Raj <aditya998887766@gmail.com>
2026-05-21 23:56:10 +0530

    step by step formation of mrge sort

---
+++ optimsesort.java
@@ -0,0 +1,16 @@
+// public class optimsesort {
+//     static void conquer(int arr[],int start[],int end,int mid){
+//         int merge[] = new int[end-start+1];
+//         int idx1 = start;
+//         int indx2 = mid+1;
+//     }
+//     static void divide(int arr[],int start,int end){
+//         if(start >= end){
+//             return;
+//         }
+//     }
+//     public static void main(String[] args) {
+//         int arr[]={6,4,6,8,9,3,2,1};
+//         int n=arr.length;
+//     }
+// }
