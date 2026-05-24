cd8d868b646243c6647ce88c27c047e257728680
Aditya Raj <aditya998887766@gmail.com>
2026-05-24 15:36:03 +0530

    basic oflinkedlist

---
+++ basiclinkedlist.java
@@ -0,0 +1,36 @@
+// class node{
+//     int val;
+//     node next;
+
+// }
+// public class basiclinkedlist {
+//     public static void main(String[] args) {
+//           node a= new node();
+//           a.val=10;
+
+//           node  b=new node();
+//           b.val=200;
+
+
+//           node c= new node();
+//           c.val=30;
+
+//           node d= new node();
+//           d.val=40;
+
+//           node e= new node();
+//           e.val=50;
+
+
+//           a.next=b;
+//            b.next=c;
+//             c.next=d;
+//              d.next=e;
+
+//              System.out.println(a.next);
+//     }
+// }
+
+
+
+
