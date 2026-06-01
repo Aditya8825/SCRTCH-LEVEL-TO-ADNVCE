3507d63fb1befcc1b86a116850d8d340a04f3ec1
Aditya Raj <aditya998887766@gmail.com>
2026-06-01 21:40:17 +0530

    operation on stack in java

---
+++ stackclass.java
@@ -0,0 +1,20 @@
+public class stackclass{
+    static class node{
+        int val;
+        node next;
+
+        public node(int data){
+            this.val=data;
+            this.next=null;
+        }
+
+        }
+        static class stack{
+            public static node head;
+            p
+        }
+    }
+
+public class operaation {
+    
+}
