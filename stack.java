85c88a841325c33b423db11c5d43dd8d6fff7e59
Aditya Raj <aditya998887766@gmail.com>
2026-06-06 15:45:18 +0530

    basic code of stack

--- stack.java
+++ stack.java
@@ -1,20 +1,29 @@
-import java.util.Stack;
+// import java .util.*;
+// public class stack {
+//     public static void main(String[] args) {
+//         Stack<Integer>s= new Stack<>();
 
-public class stack{
-    public static void main(String[] args) {
-        Stack<String>st=new Stack<>();
-        st.push("aditya");
-        st.push("adi");
-        st.push("mukesh");
-        st.push("adityaraj");
-           System.out.println(st.size());
-        System.out.println(st);
+//         //pushs element
+//         s.push(10);
+//         s.push(20);
+//         s.push(30);
 
-        System.out.println(" after popping element ");
-        st.pop();
-        
-        System.out.println(st+" "+st.size());
-        System.out.println(st.peek());
+//         //print sttack 
+//         System.out.println(" stack: "+s);
 
-    }
-}
+//          //peep top element
+//          System.out.println("top element: "+s.peek());
+
+//          //pop element
+//          System.out.println("popped : "+s.pop());
+
+//          System.out.println("stack after pop: "+s);
+
+//          //check for empty
+//          System.out.println("is stack empty "+ s.isEmpty());
+
+//          //size of stack 
+//          System.out.println("size : "+s.size());
+
+//         }
+// }
