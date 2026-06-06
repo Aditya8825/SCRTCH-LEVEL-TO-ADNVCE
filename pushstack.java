91f6dc2e302c1b3e215f69a52c1d17f0b5214749
Aditya Raj <aditya998887766@gmail.com>
2026-06-06 15:48:49 +0530

    push element in stack

---
+++ pushstack.java
@@ -0,0 +1,24 @@
+
+// import java.util.*;
+// public class pushstack {
+//     public static void main(String[] args) {
+//         Stack<Integer>st = new Stack<>();
+//         st.push(10);
+//         st.push(20);
+//         st.push(30);
+//         st.push(40);
+
+//         int ele =50;
+//         System.out.println(st);
+//         pushstack(st,ele);
+//     }
+//     public static void pushstack(Stack<Integer> st,int ele) {
+//              if(st.size()==0){
+//                 st.push(ele);
+//                 return;
+//              }
+//              int top=st.pop();
+//              pushstack(st,ele);
+//              st.push(top);
+//     }
+// }
