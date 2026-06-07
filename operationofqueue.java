94d7b74d2d1a85f48a1e76722ef55864aac4fdfd
Aditya Raj <aditya998887766@gmail.com>
2026-06-07 23:58:32 +0530

    opeartion of queue

---
+++ operationofqueue.java
@@ -0,0 +1,25 @@
+// import java.util.*;
+// import java.util.concurrent.LinkedBlockingDeque;
+// import java.util.stream.Gatherer.Integrator;
+
+// public class operationofqueue {
+//      static void  display(Queue<Integer>q){
+//         int n=q.size();
+//         for(int i=1; i<n; i++){
+//             System.out.println(q.peek());
+//             q.add(q.remove());
+//         }
+//         System.out.println();
+//      }
+//      public static void main(String[] args) {
+//         Queue<Integer>q= new LinkedBlockingDeque<>();
+//         q.add(1);
+//         q.add(2);
+//         q.add(3);
+//         q.add(4);
+//         q.add(5);
+//         display(q);
+
+        
+//      }
+// }
