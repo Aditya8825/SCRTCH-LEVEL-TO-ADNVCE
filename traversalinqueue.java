1e30f2d82180a08878e1f76dd37a72d891ed3501
Aditya Raj <aditya998887766@gmail.com>
2026-06-07 23:59:09 +0530

    traversing in queue

---
+++ traversalinqueue.java
@@ -0,0 +1,43 @@
+// import java.util.*;
+// // public class traversalinqueue {
+// //     public static void main(String[] args) {
+// //         Queue<Integer>q= new LinkedList<>();
+// //         q.add(10);
+// //          q.add(20);
+// //          q.add(30);
+// //           q.add(40);
+// //            q.add(50);
+// //            int n=q.size();
+// //            for(int i=1; i<=n; i++){
+// //             System.out.println(q.peek()+" ");
+// //            q.add(q.remove());
+// //            }
+
+// //     }
+// // }
+
+
+
+
+
+// public class traversalinqueue {
+//     static void display(Queue<Integer>q){
+//         int n=q.size();
+//         for(int i=1; i<=n; i++){
+//             System.out.println(q.peek());
+//             q.add(q.remove());
+//         }
+//         System.out.println();
+//     }
+//     public static void main(String[] args) {
+//         Queue<Integer>q= new LinkedList<>();
+//         q.add(10);
+//          q.add(20);
+//          q.add(30);
+//           q.add(40);
+//            q.add(50);
+//     display(q);
+//            }
+
+//     }
+
