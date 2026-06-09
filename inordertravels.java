531dc62b673365f0e944697e9e08b4017bf25126
Aditya Raj <aditya998887766@gmail.com>
2026-06-09 23:29:59 +0530

    inorder travels in binary tree

---
+++ inordertravels.java
@@ -0,0 +1,35 @@
+
+// class node{
+//     int val;
+//     node left;
+//     node right;
+
+//     node(int val){
+//         this.val=val;
+
+//     }
+// }
+
+// public class inordertravels {
+//     public static void main(String[] args) {
+//            node a = new node(20);
+//         node b = new node(10);
+//         node c = new node(30);
+//         node d = new node(40);
+//         node e = new node(50);
+
+//           a.left=b;
+//             a.right = c;
+//             b.left = d;
+//            b.right = e;
+//            inorder(a);
+//     }
+//     public static void inorder(node root){
+//    if(root==null) return;
+
+//    inorder(root.left);
+//     System.out.println(root.val+" ");
+//     inorder(root.right);
+//     }
+//     }
+
