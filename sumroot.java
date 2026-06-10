87aeb7ef3ecfdc041c9d4430baf1b61ebd24710f
Aditya Raj <aditya998887766@gmail.com>
2026-06-10 23:01:24 +0530

    sum of root of binary tree

---
+++ sumroot.java
@@ -0,0 +1,28 @@
+
+
+// class node{
+//     int val;
+//     node head;
+//     node left;
+//     node right;
+
+//     node(int val){
+//         this.val=val;
+//     }
+// }
+
+// public class sumroot{
+//     static  int sum(node root){
+//         if(root==null){
+//             return 0;
+//         }
+//        return root.val+sum(root.right)+sum(root.left);
+//     }
+//     public static void main(String[] args) {
+//         node n= new node(10);
+//         n.left= new node(5);
+//         n.right= new node(15);
+
+//         System.out.println(sum(n));
+//     }
+// }
