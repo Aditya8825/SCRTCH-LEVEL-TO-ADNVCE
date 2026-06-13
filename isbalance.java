0acd466442c4f11cc2c86e76105caf606c89e546
Aditya Raj <aditya998887766@gmail.com>
2026-06-13 23:07:38 +0530

    isbalance of two tree

---
+++ isbalance.java
@@ -0,0 +1,26 @@
+
+// class node{
+//     int val;
+//     node left,right;
+
+//     node(int val){
+//         this.val=val;
+//         left=right=null;
+//     }
+// }
+// public class isbalance {
+//     static boolean balance(node root){
+//         if(root==null){
+//             checkheight(root)!=-1;
+//         }
+//         static int checkheight(node root){
+//             if(root==null){
+//                 return 0;
+//             }
+//             int leftheight= checkheight(root.left);
+//             if(leftheight==-1){
+//                 return -1;
+//             }
+//                 }
+//     }
+// }
