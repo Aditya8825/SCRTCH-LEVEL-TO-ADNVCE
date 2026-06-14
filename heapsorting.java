854e4e72e86be8e29422277a2ae0e186c3f8b9d1
Aditya Raj <aditya998887766@gmail.com>
2026-06-14 23:17:49 +0530

    heap sorting

---
+++ heapsorting.java
@@ -0,0 +1,19 @@
+// import java.util.*;
+
+// public class heapsorting {
+//     public static void main(String[] args) {
+//         Scanner sc= new Scanner(System.in);
+//         System.out.print("enter size of heap tree:  ");
+//         int n= sc.nextInt();
+ 
+//         PriorityQueue<Integer>heap= new PriorityQueue<>();
+//         System.out.print("enter element :");
+//           for(int i=0; i<n; i++){
+//             heap.add(sc.nextInt());
+//           }
+//           System.out.println("sorted element:");
+//           while(!heap.isEmpty()){
+//             System.out.println(heap.poll()+" ");
+//           }
+//     }
+// }
