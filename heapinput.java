ed0a90221171e6bde74a8b896622cbbf048b90a4
Aditya Raj <aditya998887766@gmail.com>
2026-06-14 23:17:24 +0530

    heap input

---
+++ heapinput.java
@@ -0,0 +1,21 @@
+// import java.nio.channels.Pipe.SourceChannel;
+// import java.util.*;
+
+// public class heapinput {
+//    public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+
+//     System.out.println("enter number of element: ");
+//     int n= sc.nextInt();
+
+//     PriorityQueue<Integer>minHeap= new PriorityQueue<>();
+//     PriorityQueue<Integer>maxHeap=
+//     new PriorityQueue<>(Collections.reverseOrder());
+            
+//     System.out.println("enter element: ");
+
+//     for(int i=0;)
+//    } 
+// }
+
+
