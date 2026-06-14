87a776fdc5a214c7ff20d2ce31fdd6d43b1a5d25
Aditya Raj <aditya998887766@gmail.com>
2026-06-14 23:19:40 +0530

    maxheap tree

---
+++ maxheap.java
@@ -0,0 +1,16 @@
+// import java.util.*;
+
+// public class maxheap {
+//     public static void main(String[] args) {
+//         PriorityQueue<Integer>maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+//         maxHeap.add(10);
+//         maxHeap.add(5);
+//         maxHeap.add(20);
+//         maxHeap.add(2);
+//         while(!maxHeap.isEmpty()){
+//             System.out.println(maxHeap.remove()+" ");
+//             System.out.println(maxHeap.peek());
+//       //  heap.poll();
+//        }
+//     }
+// }
