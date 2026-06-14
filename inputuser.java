c4f928c1a99181ca882a6ef0da11b23a320389a2
Aditya Raj <aditya998887766@gmail.com>
2026-06-14 23:18:42 +0530

    basic of input

---
+++ inputuser.java
@@ -0,0 +1,22 @@
+// import java.util.*;
+
+// public class inputuser{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+
+        
+//     PriorityQueue<Integer>minHeap= new PriorityQueue<>();
+
+//     System.out.print("enter number of element:  ");
+//     int n=sc.nextInt();
+
+//     for(int i=0; i<n; i++){
+//         minHeap.add(sc.nextInt());
+//     }
+//     System.out.println("minimum element : "+ minHeap.peek());
+//     while(!minHeap.isEmpty()){
+//         System.out.println(minHeap.poll()+" ");
+//     }
+
+//     }
+// }
