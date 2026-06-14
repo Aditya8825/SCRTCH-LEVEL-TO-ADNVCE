01d091a63b9df24e87751e29b3d846dfa0b3e97e
Aditya Raj <aditya998887766@gmail.com>
2026-06-14 23:20:03 +0530

    minheap sorted

---
+++ minheap.java
@@ -0,0 +1,19 @@
+
+// import java.util.*;
+// public class minheap{
+//     public static void main(String[] args) {
+
+//         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+//         minHeap.add(10);
+//         minHeap.add(5);
+//         minHeap.add(20);
+//         minHeap.add(2);
+
+//        // System.out.println("Min Element: " + minHeap.peek());
+
+//         while (!minHeap.isEmpty()) {
+//             System.out.print(minHeap.remove() + " ");
+//         }
+//     }
+// }
