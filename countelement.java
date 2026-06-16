2bb7d5fd51166b8d18933e20bd8d09906f1572da
Aditya Raj <aditya998887766@gmail.com>
2026-06-16 23:53:05 +0530

    countelement

---
+++ countelement.java
@@ -0,0 +1,32 @@
+// import java.util.HashSet;
+
+// public class countelement {
+//     static int distinct(int arr[]){
+//         HashSet<Integer>set= new HashSet<>();
+//         for(int ele:arr)  set.add(ele);
+//         return set.size();
+
+//     }
+// }
+
+
+// import java.util.HashSet;
+
+// public class countelement {
+
+//     static int distinct(int arr[]) {
+//         HashSet<Integer> set = new HashSet<>();
+
+//         for (int ele : arr) {
+//             set.add(ele);
+//         }
+
+//         return set.size();
+//     }
+
+//     public static void main(String[] args) {
+//         int arr[] = {10, 20, 10, 30, 20, 40, 50, 40};
+
+//         System.out.println("Number of distinct elements: " + distinct(arr));
+//     }
+// }
