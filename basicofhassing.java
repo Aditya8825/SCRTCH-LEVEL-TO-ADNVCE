5a94933697cbc7a8f8245d96698247237a6ef9af
Aditya Raj <aditya998887766@gmail.com>
2026-06-16 23:52:40 +0530

    basic of hassing

---
+++ basicofhassing.java
@@ -0,0 +1,18 @@
+// import java.util.*;
+// public class basicofhassing{
+//    public static void main(String[] args) {
+//     HashSet<Integer > set= new HashSet<>();
+//     set.add(777);
+//     set.add(65);
+//     set.add(21);
+//     set.add(2);
+//     set.add(11);
+//     set.add(45);
+//     set.add(3);
+//     System.out.println(set.size());
+//     System.out.println(set.contains(21)+" "+set.contains(18));
+//     set.remove(21);
+//     System.out.println(set.contains(21));
+//     System.out.println(   );
+//    } 
+// }
