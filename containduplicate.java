5153353d9744b9f42439afa596929a4460ad8d47
Aditya Raj <aditya998887766@gmail.com>
2026-06-17 22:53:33 +0530

    contain doubplicate elment

---
+++ containduplicate.java
@@ -0,0 +1,18 @@
+// import java.util.HashSet;
+
+// public class containduplicate {
+//     static boolean containduplicate(int arr[]){
+//        HashSet<Integer>set= new HashSet<>();
+//         for(int num:arr){
+//             //if already prsent ,dubplicate found
+//            if(set.contains(num)){
+//             }
+//              set.add(num);
+//         }
+//         return false;
+//     }
+//    public static void main(String[] args) {
+//           int arr[]={1,3,24,3};
+//           System.out.println(containduplicate(arr));
+//    } 
+// }
