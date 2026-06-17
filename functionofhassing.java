f318714e2df175c3a43738a247990133cf9642bb
Aditya Raj <aditya998887766@gmail.com>
2026-06-17 22:55:17 +0530

    basic of hassing function

---
+++ functionofhassing.java
@@ -0,0 +1,14 @@
+// import java.util.HashMap;
+
+// public class functionofhassing {
+//     public static void main(String[] args) {
+//         HashMap<String,Integer>map= new HashMap<>();
+//         map.put("aditya ",20);
+//         map.put("aabhinav ",35);
+//         System.out.println(map+" "+map.size());
+//         System.out.println(map.remove("vivek"));
+//         System.out.println(map.get("raghav"));
+//         map.put("deepika",20);
+//         System.out.println(map+" "+map.size());
+//     }
+// }
