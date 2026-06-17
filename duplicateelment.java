ae44900aa90f2d8912bdc48e77a0c7076f916a82
Aditya Raj <aditya998887766@gmail.com>
2026-06-17 22:54:32 +0530

    dublicate element.java

---
+++ duplicateelment.java
@@ -0,0 +1,15 @@
+// import java.util.HashSet;
+
+// public class duplicateelment {
+//     public static void main(String[] args) {
+//         int arr[]={1,2,3,4,5,4,6};
+//         HashSet<Integer>set= new HashSet<>();
+
+//         //if add() return false,the lement is dubplicate
+//         for(int num:arr){
+//             if(!set.add(num)){
+//                 System.out.println("duplicte "+num);
+//             }
+//         }
+//     }
+// }
