4373bc42c19c54806aeca6a3de3cfc08220f7691
Aditya Raj <aditya998887766@gmail.com>
2026-06-19 23:45:56 +0530

    maping

---
+++ map.java
@@ -0,0 +1,20 @@
+// import java.util.HashSet;
+
+// public class map {
+//    public static void main(String[]args){
+//     HashSet<Integer>set= new HashSet<>();
+
+//     set.add(20);
+//     set.add(40);
+//     set.add(50);
+//     set.add(70);
+
+//     // for(int ele:set){
+//     //     set.remove(ele);
+//     // }
+//     System.out.println();
+//       for(int ele:set){
+//         System.out.print(ele+" ");
+//       }
+//    }     
+// }
