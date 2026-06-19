d7a5531fb875ca0abb77e6397295700df5552786
Aditya Raj <aditya998887766@gmail.com>
2026-06-19 23:43:54 +0530

    countdistinct

---
+++ countdistinct.java
@@ -0,0 +1,14 @@
+// import java.util.*;
+// public class countdistinct {
+//     public static void main(String[] args) {
+//         int arr[]={1,2,2,3,14,5};
+//         HashMap<Integer,Integer>map=new HashMap<>();
+
+//         for(int num:arr){
+//             if(!map.containsKey(num)){
+//                 map.put(num,1);
+//             }
+//         }
+//         System.out.println("distinct num: "+map.size());
+//     }
+// }
