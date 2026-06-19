cfbf16bfd5717572ad3d5c4119ef9b7d82ffe69b
Aditya Raj <aditya998887766@gmail.com>
2026-06-19 23:44:21 +0530

    countfrequncy

---
+++ countfrequency.java
@@ -0,0 +1,33 @@
+// import java.util.HashMap;
+// import java.util.Map;
+
+// public class countfrequency {
+//   public static void main(String[] args) {
+// //     int arr[]={1,2,1,3,2,1};
+// //     HashMap<Integer,Integer>map= new HashMap<>();
+
+// //     for(int num: arr){
+// //         map.put(num,map.getOrDefault(num,0));
+// //     }
+// //     System.out.println(map);
+// //   }  
+// // }
+
+
+// String str="leetcode";
+// HashMap<Character,Integer>map= new HashMap<>();
+// for(int i=0; i<str.length(); i++){
+//     char ch=str.charAt(i);
+// Integer count=map.get(ch);
+
+// if(count==null)
+//   map.put(ch,1);
+// else
+//     map.put(ch,1);
+// }
+// for(Map.Entry<Character,Integer> entry:map.entrySet()){
+//     System.out.println(entry.getKey()+"->"+
+//      entry.getValue());
+// }
+//   }
+// }
