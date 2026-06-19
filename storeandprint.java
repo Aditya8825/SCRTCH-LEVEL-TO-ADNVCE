af2dbde4fcbc441fe91c1363f8ba47031bba3562
Aditya Raj <aditya998887766@gmail.com>
2026-06-19 23:46:53 +0530

    store and print the values

---
+++ storeandprint.java
@@ -0,0 +1,14 @@
+// import java.util.HashMap;
+
+// public class storeandprint {
+//    public static void main(String[] args) {
+//     HashMap<String,Integer>map= new HashMap<>();
+
+//     map.put("aditya",85);
+//     map.put("Areesha",90);
+//     map.put("Alsifa",98);
+
+//     System.out.println(map);
+    
+//    } 
+// }
