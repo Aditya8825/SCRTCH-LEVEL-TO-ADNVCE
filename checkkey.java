b04c92f7721233f608723f43d3d715d63214ea5a
Aditya Raj <aditya998887766@gmail.com>
2026-06-19 23:43:27 +0530

    check present of key using map

---
+++ checkkey.java
@@ -0,0 +1,15 @@
+// import java.util.HashMap;
+
+// public class checkkey {
+//    public static void main(String[] args) {
+//     HashMap<String,Integer>map= new HashMap<>();
+//     map.put("Aditya",90);
+//     map.put("Rahul",90);
+//     map.put("dipu",99);
+//     if(map.containsKey("aditya")){
+//         System.out.println("student  found");
+//     }else{
+//         System.out.println("student not found");
+//     }
+//    } 
+// }
