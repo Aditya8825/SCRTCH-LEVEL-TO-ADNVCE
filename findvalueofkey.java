6734bb4918637e7b704aa077ec2cc5b3ce029512
Aditya Raj <aditya998887766@gmail.com>
2026-06-19 23:45:12 +0530

    find the value of key

---
+++ findvalueofkey.java
@@ -0,0 +1,9 @@
+// import java.util.HashMap;
+
+// public class findvalueofkey {
+//     public static void main(String[] args) {
+//         HashMap<String,Integer>map= new HashMap<>();
+//         map.put("Aditya",90);
+//         System.out.println(map.get("Rahul"));
+//     }
+// }
