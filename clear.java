73b31d5808a903c0e9b48c3a471da5d38da5e566
Aditya Raj <aditya998887766@gmail.com>
2026-06-20 23:25:48 +0530

    clear bit

---
+++ clear.java
@@ -0,0 +1,10 @@
+
+// public class clear {
+//  public static void main(String[] args) {
+//     int n =13;
+//     int i=2;
+
+//      int ans=n&~(1<<i);
+//      System.out.println(ans);
+//      }   
+// }
