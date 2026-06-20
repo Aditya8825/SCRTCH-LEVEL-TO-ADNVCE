b55b0cc80dd7b5f6c91e215ad153e1fdbe071601
Aditya Raj <aditya998887766@gmail.com>
2026-06-20 23:26:42 +0530

    getithub

---
+++ getithbit.java
@@ -0,0 +1,13 @@
+
+// public class getithbit {
+//    public static void main(String[] args) {
+//     int n=13;
+//     int i=2;
+//       int mask=1<<i;
+//     if((n & mask)!=0){
+//         System.out.println(" bit is 1 ");
+//     }else{
+//         System.out.println(" bit is 0 ");
+//     }
+//    } 
+// }
