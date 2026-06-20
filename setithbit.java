ae88db61ff632d2e0b78a2c0ae5b17b6f60702aa
Aditya Raj <aditya998887766@gmail.com>
2026-06-20 23:27:08 +0530

    sitbit

---
+++ setithbit.java
@@ -0,0 +1,11 @@
+
+// public class setithbit {
+//   public static void main(String[] args) {
+//     int n=10;
+//     int i=0;
+
+
+//     int ans=n|(1<<i);
+//     System.out.println(ans);
+//   }  
+// }
