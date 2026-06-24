140a57670798acbf038ef515328d9cdf9be41c1c
Aditya Raj <aditya998887766@gmail.com>
2026-06-24 17:42:49 +0530

    min cost of tree

---
+++ mintree.java
@@ -0,0 +1,11 @@
+// public class mintree {
+//      static int mincost(int i, int[] cost){
+//         // based cased 
+//         if(i>=cost.length) return 0;
+//      }
+//      return cost[i]+Math.min(mincost(i+1,cost),
+//      mincost());
+//     public static void main(String[] args) {
+//         int [] cost={10,15,20};
+//     }
+// }
